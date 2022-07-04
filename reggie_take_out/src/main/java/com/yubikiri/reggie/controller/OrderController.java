95e995b8e16c79506cdf7396ca8f413d046843bb
YUBIKIRI520 <648308255@qq.com>
2022-07-04 01:15:29 -0400

    POST '/order/submit'

--- reggie_take_out/src/main/java/com/yubikiri/reggie/controller/OrderController.java
+++ reggie_take_out/src/main/java/com/yubikiri/reggie/controller/OrderController.java
@@ -1,7 +1,11 @@
 package com.yubikiri.reggie.controller;
 
+import com.yubikiri.reggie.common.R;
+import com.yubikiri.reggie.entity.Orders;
 import com.yubikiri.reggie.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +17,12 @@ public class OrderController {
 
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
+    }
+
+    @PostMapping("/submit")
+    public R<String> submit(@RequestBody Orders orders) {
+
+        orderService.submit(orders);
+        return R.success("订单创建成功");
     }
 }
--- reggie_take_out/src/main/java/com/yubikiri/reggie/service/OrderService.java
+++ reggie_take_out/src/main/java/com/yubikiri/reggie/service/OrderService.java
@@ -4,4 +4,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.yubikiri.reggie.entity.Orders;
 
 public interface OrderService extends IService<Orders> {
+
+    public void submit(Orders orders);
 }
